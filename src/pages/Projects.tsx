e0e29fc8ad41940ac0795d439aaaf15c834fc1a8
Manoj <manojkumarbonala3@gmail.com>
2024-04-17 20:54:00 +0530

    code

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,46 +1,35 @@
 import maroon from "@/assets/maroon.png";
-import { CardBody, CardContainer, CardItem } from "@/components/Card";
-// import MotionDiv from "@/components/motion-div";
+import { CardContainer } from "@/components/Card";
 
 export default function Projects() {
   return (
     <section>
       <h1 className="text-center text-3xl">Projects</h1>
       <div>
-        <div>
-          <CardContainer>
-            <CardBody>
-              <CardItem>
-                <img
-                  src={maroon}
-                  alt="photo"
-                  className="w-full h-full rounded-lg"
-                />
-              </CardItem>
-            </CardBody>
+        <div className="flex justify-center">
+          <CardContainer className="border border-slate-200 rounded-lg shadow-lg dark:border-none">
+            <div>
+              <img
+                src={maroon}
+                alt="photo"
+                height="1000"
+                width="1000"
+                className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+              />
+            </div>
           </CardContainer>
-          <div className="px-12 space-y-3">
-            <h1 className="text-xl font-semibold">Maroon Marketing Agency</h1>
-            <p className="w-[400px] text-slate-400">
-              Maroon Marketing Agency is a digital marketing agency that offers
-              a suite of marketing services to help brands grow their business.
-            </p>
+          <div className="px-12  flex justify-center pt-24">
+            <div className="space-y-3">
+              <h1 className="text-xl font-semibold">Maroon Marketing Agency</h1>
+              <p className="w-[400px] text-slate-400">
+                Maroon Marketing Agency is a digital marketing agency that
+                offers a suite of marketing services to help brands grow their
+                business.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-
-      {/* <div className="flex justify-center px-12 py-10">
-        <CardContainer>
-        <img src={maroon} alt="photo" className="w-5/12 rounded-lg" />
-        </CardContainer>
-        <div className="px-12 space-y-3">
-          <h1 className="text-xl font-semibold">Maroon Marketing Agency</h1>
-          <p className="w-[400px] text-slate-400">
-            Maroon Marketing Agency is a digital marketing agency that offers a
-            suite of marketing services to help brands grow their business.
-          </p>
-        </div>
-      </div> */}
     </section>
   );
 }
