62fcb834fc141e14989970393370b2a800f28059
Manoj <manojkumarbonala3@gmail.com>
2024-05-02 07:15:34 +0530

    code

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,21 +7,21 @@ export default function About() {
     <>
       <section
         id="about"
-        className="mx-auto my-16 flex flex-col items-center justify-center gap-4 px-32 md:my-56 lg:pt-20  md:max-w-full lg:flex-row lg:items-start lg:gap-16"
+        className="mx-auto my-16 flex flex-col items-center justify-center gap-4 px-4 lg:px-32 md:my-56 lg:pt-20  md:max-w-full lg:flex-row lg:items-start lg:gap-16"
       >
         <div className="order-2 lg:order-1 lg:w-2/3 ">
           <MotionDiv delayOffset={0.2}>
             <h2 className="mb-3 w-full text-center md:mb-14 text-xl">About Me</h2>
           </MotionDiv>
-          <article className="flex flex-col gap-4">
+          <article className="flex flex-col gap-4 pt-24 lg:pt-0">
             <MotionDiv delayOffset={0.4}>
-              <p>
+              <p className="pl-4 w-[320px] lg:w-full">
                 Hello, I'm Manoj Kumar. I'm an independent <b>TypeScript Full-Stack</b> developer
                 based in India.
               </p>
             </MotionDiv>
             <MotionDiv delayOffset={0.6}>
-              <p className="pl-2">
+              <p className="pl-4 w-[320px] lg:w-full">
                 As a technologies enthusiast, I am highly passionate about
                 building full-stack web applications. And I love learning new
                 technologies, especially elegant code architectures and design
@@ -32,9 +32,9 @@ export default function About() {
           </article>
         </div>
         <div className="lg:order-2 lg:w-1/3 lg:pr-40">
-          <MotionDiv delayOffset={0.7}>
+          <MotionDiv delayOffset={0.3}>
             <CardContainer>
-              <CardBody className="pl-6 lg:block">
+              <CardBody className="pl-6 lg:block flex justify-center items-center pt-20 lg:pt-0 pr-6">
                 <img
                   src={photo}
                   alt="photo"
