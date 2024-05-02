54861c6e1f1ad7075e6737a653f31e66c6f8e568
Manoj <manojkumarbonala3@gmail.com>
2024-05-02 07:38:57 +0530

    code

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,40 +9,44 @@ export default function About() {
         id="about"
         className="mx-auto my-16 flex flex-col items-center justify-center gap-4 px-4 lg:px-32 md:my-56 lg:pt-20  md:max-w-full lg:flex-row lg:items-start lg:gap-16"
       >
-        <div className="order-2 lg:order-1 lg:w-2/3 ">
+        <div className="order-2 lg:order-1 lg:w-2/3">
           <MotionDiv delayOffset={0.2}>
-            <h2 className="mb-3 w-full text-center md:mb-14 text-xl">About Me</h2>
+            <h2 className="mb-3 w-full text-center md:mb-14 text-xl lg:pt-24">
+              About Me
+            </h2>
           </MotionDiv>
-          <article className="flex flex-col gap-4 pt-24 lg:pt-0">
-            <MotionDiv delayOffset={0.4}>
-              <p className="pl-4 w-[320px] lg:w-full">
-                Hello, I'm Manoj Kumar. I'm an independent <b>TypeScript Full-Stack</b> developer
-                based in India.
-              </p>
-            </MotionDiv>
-            <MotionDiv delayOffset={0.6}>
-              <p className="pl-4 w-[320px] lg:w-full">
-                As a technologies enthusiast, I am highly passionate about
-                building full-stack web applications. And I love learning new
-                technologies, especially elegant code architectures and design
-                patterns. I hope to leverage my technical skills to build
-                valuable apps that can make a difference in people's lives.
-              </p>
-            </MotionDiv>
-          </article>
-        </div>
-        <div className="lg:order-2 lg:w-1/3 lg:pr-40">
-          <MotionDiv delayOffset={0.3}>
-            <CardContainer>
-              <CardBody className="pl-6 lg:block flex justify-center items-center pt-20 lg:pt-0 pr-6">
-                <img
-                  src={photo}
-                  alt="photo"
-                  className="w-[350px] min-w-[300px] rounded-xl transition-all"
-                />
-              </CardBody>
-            </CardContainer>
-          </MotionDiv>
+          <div className="flex gap-20 flex-col lg:flex-row pt-0 mt-0">
+            <div className="lg:order-2 lg:w-1/3 lg:pr-40 lg:pt-0">
+              <MotionDiv delayOffset={0.4}>
+                <CardContainer>
+                  <CardBody className="pl-6 lg:block flex justify-center items-center pt-44 lg:pt-0 pr-6">
+                    <img
+                      src={photo}
+                      alt="photo"
+                      className="w-[350px] min-w-[300px] rounded-xl transition-all"
+                    />
+                  </CardBody>
+                </CardContainer>
+              </MotionDiv>
+            </div>
+            <article className="flex flex-col gap-4 pt-28 lg:pt-12">
+              <MotionDiv delayOffset={0.5}>
+                <p className="pl-4 w-[320px] lg:w-full">
+                  Hello, I'm Manoj Kumar. I'm an independent{" "}
+                  <b>TypeScript Full-Stack</b> developer based in India.
+                </p>
+              </MotionDiv>
+              <MotionDiv delayOffset={0.6}>
+                <p className="pl-4 w-[320px] lg:w-full">
+                  As a technologies enthusiast, I am highly passionate about
+                  building full-stack web applications. And I love learning new
+                  technologies, especially elegant code architectures and design
+                  patterns. I hope to leverage my technical skills to build
+                  valuable apps that can make a difference in people's lives.
+                </p>
+              </MotionDiv>
+            </article>
+          </div>
         </div>
       </section>
     </>
